4f055d46f7784d2ef5f382dc2a772791d76d1473
joe <kinjoyuji@gmail.com>
2019-04-09 17:04:47 +1000

    terraform init

---
+++ main.tf
@@ -0,0 +1,13 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
